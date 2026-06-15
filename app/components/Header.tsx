bcb2fc1254e80b6fac73353042497a533e2fe179
CryptoBidDev <chris@reccemarketing.com>
2026-06-15 14:43:32 +0400

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -72,7 +72,7 @@ export default function Header() {
 
               <div className="min-w-0 overflow-hidden">
                 <p className="truncate text-lg sm:text-3xl font-black tracking-tight leading-none">
-                  Crypto
+                  CryptoTEST
                   <span className="bg-gradient-to-r from-cyan-400 via-blue-500 to-fuchsia-500 bg-clip-text text-transparent">
                     BidX
                   </span>
